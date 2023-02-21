d7550951085e2692f841f9cec8560a7c04415747
ankursoniblogger <ankursoniblogger@gmail.com>
2023-02-21 18:13:23 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,21 @@
+// primitve data type store actual value of varible 
+/*
+var a = 10;
+
+var b = a;
+
+b = 20 ;
+
+console.log("a:" , a );  */
+
+// Non primitive data type hold memory location/address of value of varible
+
+/*
+var b = { name: 'Ankur'} 
+
+var c = b ;
+c.name = "Ankur soni";
+
+console.log("b:" , b) */
+
+  

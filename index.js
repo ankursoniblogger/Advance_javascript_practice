5825e73bb665fb3fbd6e443d441fd4b1442dd2da
ankursoniblogger <ankursoniblogger@gmail.com>
2023-02-22 21:05:27 +0530

    Days 2 in advance js

--- index.js
+++ index.js
@@ -28,3 +28,29 @@ console.log(arr);
 var str = "Ankur";
 str[0] = "a";
 console.log(str) */
+/*
+var admin = {
+    name: "Ankur",
+    password: "ankur12",
+    age: "19"
+}
+
+// it's can be change admin 
+admin = {
+    name: "ankit",
+    password: "ankit12",
+    age: "21"
+}
+console.log(admin) */
+
+// But if I written objuct.freeze() ,can't change admin 
+/*
+var admin = {
+    name: "Ankur",
+    password: "ankur12",
+    age: "19"
+}
+Object.freeze(admin);
+
+admin.name = "Ankit soni";  //ab admin freez ho gya hain esse change nai kr sakte
+console.log(admin) */
